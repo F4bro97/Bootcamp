2eab140e99cb7014bbb7b02f076a29bf0fb53ff5
Fabrizio Afonso <fabrizioafonso@gmail.com>
2023-09-26 16:34:37 -0300

    Add Conditionals

--- script.sh
+++ script.sh
@@ -3,6 +3,64 @@
 
 #funciones 
 
+    #Registrar nombre y apellido con Condicion de registro
+
+IngresarNuevoUsuario(){
+
+    #Ingresar nombre
+while true 
+    do
+        read -r -p "Ingrese Nombre:" NombreP
+        if [[  "$NombreP" =~ ^[a-zA-Z]+$ ]]
+            then
+                break # Esto sirve para que salga del loop cuando sea valido
+        else
+            echo "El nombre solo puede contener letras, Intentelo denuevo."
+        fi
+done
+    echo Nombre valido!
+
+    #Ingresar apellido
+while true 
+    do
+        read -r -p "Ingrese Apellido:" ApellidoP
+        if [[  "$ApellidoP" =~ ^[a-zA-Z]+$ ]]
+            then
+                break # Esto sirve para que salga del loop cuando sea valido
+        else
+            echo "El apellido solo puede contener letras, Intentelo denuevo."
+        fi
+done
+    echo Apellido valido!
+
+    #Ingresar mail
+while true
+    do
+        read -r -p "Ingrese Mail:" MailP
+        if [[  "$MailP" =~ ^[a-zA-Z0-9,@]+$ ]]
+            then
+                break # Esto sirve para que salga del loop cuando sea valido
+        else
+            echo "El mail debe contener este formato 'usuario@mail.com', Intentelo denuevo."
+        fi
+done
+    echo Email valido!
+
+    #Ingresar Telefono
+while true
+    do
+        read -r -p "Ingrese Telefono:" TelP
+        if [[  "$TelP" =~ ^[0-9]+$ ]]
+            then
+                break # Esto sirve para que salga del loop cuando sea valido
+        else
+            echo "El telefono debe contener solo numeros, Intentelo denuevo."
+        fi
+done
+    echo Telefono valido!
+}
+
+
     #menu de preguntas principal
 menup(){
     echo "Menu"
@@ -14,17 +72,14 @@ menup(){
 
     #Crear nuevo usuario
 crearnuevo(){
-    read -r -p "Ingrese Nombre:" NombreP ; 
-    read -r -p "Ingrese Apellido:" ApellidoP ;
-    read -r -p "Ingrese Mail:" MailP ;
-    read -r -p "Ingrese Telefono:" TelP ;
+    IngresarNuevoUsuario;
     echo "$NombreP" "$ApellidoP" "$MailP" "$TelP" >> nombre.txt && echo "usuario Ingresado con exito"
     menup;pregunta
 }
 
     #Buscar Usuario
 Buscaru(){
-    read -r -p "nombre del usuario que busca:" UsuarioB ;
+    read -r -p "Dato del usuario que busca:" UsuarioB ;
         if grep -q "$UsuarioB" nombre.txt
         then
             echo "se encontro:" 
@@ -79,6 +134,8 @@ pregunta2(){
             pregunta
         fi
 }
+
+
 
 
 
