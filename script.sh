8835d3c300ff6bc0a19b2ad2b80a003137f4f4f2
Fabrizio Afonso <fabrizioafonso@gmail.com>
2023-09-26 17:58:55 -0300

    only good practices

--- script.sh
+++ script.sh
@@ -1,143 +1,128 @@
 #!/bin/bash
 
+# Functions
 
-#funciones 
+# Name and Surname register with Conditions
+insertnewuser(){
 
-    #Registrar nombre y apellido con Condicion de registro
-
-IngresarNuevoUsuario(){
-
-    #Ingresar nombre
+# Insert Names
 while true 
-    do
-        read -r -p "Ingrese Nombre:" NombreP
-        if [[  "$NombreP" =~ ^[a-zA-Z]+$ ]]
-            then
-                break # Esto sirve para que salga del loop cuando sea valido
-        else
-            echo "El nombre solo puede contener letras, Intentelo denuevo."
-        fi
+do
+read -r -p "Ingrese Nombre:" newname
+if [[  "$newname" =~ ^[a-zA-Z]+$ ]]; then
+break # Input is valid. Exit loop
+else
+echo "El nombre solo puede contener letras, Intentelo denuevo."
+fi
 done
-    echo Nombre valido!
+echo Nombre valido!
 
-    #Ingresar apellido
+# Insert Surname
 while true 
-    do
-        read -r -p "Ingrese Apellido:" ApellidoP
-        if [[  "$ApellidoP" =~ ^[a-zA-Z]+$ ]]
-            then
-                break # Esto sirve para que salga del loop cuando sea valido
-        else
-            echo "El apellido solo puede contener letras, Intentelo denuevo."
-        fi
+do
+read -r -p "Ingrese Apellido:" newsurname
+if [[  "$newsurname" =~ ^[a-zA-Z]+$ ]];then
+break #Input is valid. Exit loop
+else
+echo "El apellido solo puede contener letras, Intentelo denuevo."
+fi
 done
-    echo Apellido valido!
+echo Apellido valido!
 
-    #Ingresar mail
+# Insert Mail
 while true
-    do
-        read -r -p "Ingrese Mail:" MailP
-        if [[  "$MailP" =~ ^[a-zA-Z0-9,@]+$ ]]
-            then
-                break # Esto sirve para que salga del loop cuando sea valido
-        else
-            echo "El mail debe contener este formato 'usuario@mail.com', Intentelo denuevo."
-        fi
+do
+read -r -p "Ingrese Mail:" newmail
+if [[  "$newmail" =~ ^[a-zA-Z0-9,@]+$ ]];then
+break #Input is valid. Exit loop
+else
+echo "El mail debe contener este formato 'usuario@mail.com', Intentelo denuevo."
+fi
 done
-    echo Email valido!
+echo Email valido!
 
-    #Ingresar Telefono
+# Insert phone number
 while true
-    do
-        read -r -p "Ingrese Telefono:" TelP
-        if [[  "$TelP" =~ ^[0-9]+$ ]]
-            then
-                break # Esto sirve para que salga del loop cuando sea valido
-        else
-            echo "El telefono debe contener solo numeros, Intentelo denuevo."
-        fi
+do
+read -r -p "Ingrese Telefono:" newtel
+if [[  "$newtel" =~ ^[0-9]+$ ]];then
+break #Input is valid. Exit loop
+else
+echo "El telefono debe contener solo numeros, Intentelo denuevo."
+fi
 done
-    echo Telefono valido!
+echo Telefono valido!
 }
 
-
-    #menu de preguntas principal
-menup(){
-    echo "Menu"
-    echo "1-Crear"
-    echo "2-Buscar"
-    echo "3-Ayuda"
-
+# Main Menu
+mainmenu(){
+echo "Menu"
+echo "1-Crear"
+echo "2-Buscar"
+echo "3-Ayuda"
 }
 
-    #Crear nuevo usuario
-crearnuevo(){
-    IngresarNuevoUsuario;
-    echo "$NombreP" "$ApellidoP" "$MailP" "$TelP" >> nombre.txt && echo "usuario Ingresado con exito"
-    menup;pregunta
+# Crear nuevo usuario
+createusermenu(){
+Insertnewuser;
+echo "$newname" "$newsurname" "$newmail" "$newtel" >> nombre.txt && echo "usuario Ingresado con exito"
+mainmenu;pregunta
 }
 
-    #Buscar Usuario
-Buscaru(){
-    read -r -p "Dato del usuario que busca:" UsuarioB ;
-        if grep -q "$UsuarioB" nombre.txt
-        then
-            echo "se encontro:" 
-            grep "$UsuarioB" nombre.txt
-            menup;pregunta
-        else
-            echo "no se encontro ningun usuario"
-            menup;pregunta
-        fi
-
+# Search users
+searchuser(){
+read -r -p "Dato del usuario que busca:" usersearch ;
+if grep -q "$usersearch" nombre.txt;then
+echo "se encontro:" 
+grep "$usersearch" nombre.txt
+mainmenu;deploymainmenu
+else
+echo "no se encontro ningun usuario"
+mainmenu;deploymainmenu
+fi
 }
-ayuda(){
+
+# Help menu
+helpmenu(){
 echo "Por asistencia remota comunicarse con @Fabro_afonso en slack c:"
-menup;pregunta
+mainmenu;deploymainmenu
 }
 
-    # if para saber que numero es
-pregunta(){
-    read -r -p "elige opcion:" var1
-        if [ "$var1" = "1" ]
-        then
-            echo "entraste en crear"
-            crearnuevo
-        elif [ "$var1" = "2" ]
-        then
-            echo "Entraste en buscar"
-            Buscaru
-        elif [ "$var1" = "3" ]
-        then
-            echo "entraste en ayuda"
-        else
-            echo "no has seleccionado una opcion que corresponda"
-            pregunta2
-        fi
+# First option catcher
+deploymainmenu(){
+read -r -p "elige opcion:" var1
+if [ "$var1" = "1" ];then
+echo "entraste en crear"
+createusermenu
+elif [ "$var1" = "2" ];then
+echo "Entraste en buscar"
+searchuser
+elif [ "$var1" = "3" ];then
+echo "entraste en ayuda"
+helpmenu
+else
+echo "no has seleccionado una opcion que corresponda"
+deploymainmenuagain
+fi
 }
 
-    # Segundo if por si no elige correcto el primero
-pregunta2(){
-    read -r -p "elige opcion:" var1
-        if [ "$var1" = "1" ]
-        then
-            echo "entraste en crear"
-            Crear
-        elif [ "$var1" = "2" ]
-        then
-            echo "Entraste en buscar"
-        elif [ "$var1" = "3" ]
-        then
-            echo "entraste en ayuda"
-        else
-            echo "no has seleccionado una opcion que corresponda"
-            pregunta
-        fi
+# Second option catcher
+deploymainmenuagain(){
+read -r -p "elige opcion:" var1
+if [ "$var1" = "1" ];then
+echo "entraste en crear"
+createusermenu
+elif [ "$var1" = "2" ];then
+echo "Entraste en buscar"
+searchuser
+elif [ "$var1" = "3" ];then
+echo "entraste en ayuda"
+helpmenu
+else
+echo "no has seleccionado una opcion que corresponda"
+deploymainmenu
+fi
 }
 
-
-
-
-
-# PROCESO DE EJECUCION PRIMARIO
-menup;pregunta
+# Main process
+mainmenu;deploymainmenu
